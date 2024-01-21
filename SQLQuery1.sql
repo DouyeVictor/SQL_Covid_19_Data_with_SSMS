94e5f5989b8a2a63c26a2315de8bc6175057a1ec
DouyeVictor <119713697+DouyeVictor@users.noreply.github.com>
2024-01-21 22:27:32 +0000

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,153 +1,34 @@
---Aurthor: Douye Victor O.
---Topic: Data Exploration of Covid 19 Data Set
---Tool Used: SSMS
---Dataset Source: https://ourworldindata.org/covid-deaths
---Date of collection: 26-06-2023
---Date of most recent update before collection: 25-06-2023
-
+--Data Exploration
 SELECT *
-FROM PortfolioProject.dbo.Covid19Deaths
-WHERE Continent is not null
-ORDER BY 3,4
+FROM CH_python$;
 
-SELECT Location, date, total_cases, new_cases, total_deaths, population
-FROM PortfolioProject.dbo.Covid19Deaths
-ORDER BY 1,2
+--Calculate average price, mileage, and tax
+SELECT AVG(price) AS avg_price, AVG(mileage) AS avg_mileage, AVG(tax) AS avg_tax
+FROM CH_python$;
 
---Looking at Total Cases vs Total Deaths
-SELECT Location, date, total_cases,total_deaths, (total_deaths *100 / total_cases) as DeathPercentage
-FROM PortfolioProject.dbo.Covid19Deaths
-WHERE location like '%Kingdom%'
-and total_cases is not null
-ORDER BY 1,2 
+-- Distribution of fuel types
+SELECT fuelType, COUNT(*) AS count
+FROM CH_python$
+GROUP BY fuelType;
 
-SELECT Location, date, population,total_cases, (total_cases / population) *100 as tp
-FROM PortfolioProject.dbo.Covid19Deaths
-ORDER BY 1,2
+-- Average price over the years
+SELECT year, AVG(price) AS avg_price
+FROM CH_python$
+GROUP BY year
+ORDER BY year;  
 
---Looking at countries with the highest infection rate compared to Population
-SELECT Location,population,MAX(total_cases) as HighestInfectionCount, Max((total_cases / population))*100 as PercentPopulationInfected
-FROM PortfolioProject.dbo.Covid19Deaths
---WHERE location like '%States%'
-GROUP BY location, population
-ORDER BY PercentPopulationInfected desc
+-- Top 5 most expensive cars
+SELECT DISTINCT TOP 5 *
+FROM CH_python$
+ORDER BY price DESC;
 
---Showing countries with the highest death count by population
-SELECT Location, Max(cast(total_deaths as int)) as TotalDeathCount
-FROM PortfolioProject.dbo.Covid19Deaths
-WHERE continent is not null
-GROUP BY location
-ORDER BY TotalDeathCount desc
+--Average Tax for Each Transmission Type
+SELECT year, SUM(tax) AS total_tax
+FROM CH_python$
+GROUP BY year
+ORDER BY year;
 
---Breakdown by continent
-SELECT continent, Max(cast(total_deaths as int)) as TotalDeathCount 
-FROM PortfolioProject.dbo.Covid19Deaths
-WHERE continent is not null
-GROUP BY continent
-ORDER BY TotalDeathCount desc
-
---Global Numbers
-SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast
-  (new_deaths as int)) / SUM(new_cases)*100 as DeathPercentage
-FROM PortfolioProject.dbo.Covid19Deaths
-WHERE continent is not null
---GROUP BY date
-ORDER BY 1,2
-
-SELECT*
-FROM PortfolioProject.dbo.Covid19Deaths as Death
-Join PortfolioProject.dbo.CovidVaccinations as Vac
- ON Death.location = Vac.location
- AND Death.date = Vac.date
-
- --Looking at total population vs Vaccination
- SELECT Death.continent, Death.location, Death.date, Death.population, Vac.new_vaccinations
- , SUM(CONVERT(bigint, Vac.new_vaccinations)) OVER (Partition by Death.location ORDER BY Death.location,
- Death.date) as RollingPeopleVaccinated
- FROM PortfolioProject.dbo.Covid19Deaths as Death
- Join PortfolioProject.dbo.CovidVaccinations as Vac
-  ON Death.location = Vac.location
-  AND Death.date = Vac.date
-WHERE Death.continent is not null
-ORDER BY 2,3
-
--- USE CTE
-With PopvsVac ( Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
-as
-(
-SELECT Death.continent, Death.location, Death.date, Death.population, Vac.new_vaccinations
- , SUM(CONVERT(bigint, Vac.new_vaccinations)) OVER (Partition by Death.location ORDER BY Death.location,
- Death.date) as RollingPeopleVaccinated
- FROM PortfolioProject.dbo.Covid19Deaths as Death
- Join PortfolioProject.dbo.CovidVaccinations as Vac
-  ON Death.location = Vac.location
-  AND Death.date = Vac.date
-WHERE Death.continent is not null
---ORDER BY 2,3
-)
-SELECT *,(RollingPeopleVaccinated/Population)*100
-FROM PopvsVac
-
---TEMP TABLE
-
-DROP TABLE if exists #PercentPopulationVaccinated
-CREATE TABLE #PercentPopulationVaccinated
-(
-Continent nvarchar(255),
-Location nvarchar (255),
-Date datetime,
-Population numeric,
-New_vaccinations numeric,
-RollingPeopleVaccinated numeric
-)
-
-INSERT INTO #PercentPopulationVaccinated
-
-SELECT Death.continent, Death.location, Death.date, Death.population, Vac.new_vaccinations
- , SUM(CONVERT(bigint, Vac.new_vaccinations)) OVER (Partition by Death.location ORDER BY Death.location,
- Death.date) as RollingPeopleVaccinated
- FROM PortfolioProject.dbo.Covid19Deaths as Death
- Join PortfolioProject.dbo.CovidVaccinations as Vac
-  ON Death.location = Vac.location
-  AND Death.date = Vac.date
---WHERE Death.continent is not null
---ORDER BY 2,3
-
-SELECT *,(RollingPeopleVaccinated/Population)*100
-FROM #PercentPopulationVaccinated
-
---Creating view to store data for data visualizations
-CREATE VIEW PercentPopulationVaccinated as
-
-SELECT Death.continent, Death.location, Death.date, Death.population, Vac.new_vaccinations
- , SUM(CONVERT(bigint, Vac.new_vaccinations)) OVER (Partition by Death.location ORDER BY Death.location,
- Death.date) as RollingPeopleVaccinated
- FROM PortfolioProject.dbo.Covid19Deaths as Death
- Join PortfolioProject.dbo.CovidVaccinations as Vac
-  ON Death.location = Vac.location
-  AND Death.date = Vac.date
-WHERE Death.continent is not null
---ORDER BY 2,3
-
-CREATE VIEW Global_Numbers as
-
-SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast
-  (new_deaths as int)) / SUM(new_cases)*100 as DeathPercentage
-FROM PortfolioProject.dbo.Covid19Deaths
-WHERE continent is not null
---GROUP BY date
---ORDER BY 1,2
-
-CREATE VIEW Infection_Rate_by_Population as
-SELECT Location,population,MAX(total_cases) as HighestInfectionCount, Max((total_cases / population))*100 as PercentPopulationInfected
-FROM PortfolioProject.dbo.Covid19Deaths
---WHERE location like '%States%'
-GROUP BY location, population
---ORDER BY PercentPopulationInfected desc
-
-CREATE VIEW UK_Cases_vs_Deaths as
-SELECT Location, date, total_cases,total_deaths, (total_deaths *100 / total_cases) as DeathPercentage
-FROM PortfolioProject.dbo.Covid19Deaths
-WHERE location like '%Kingdom%'
-and total_cases is not null
---ORDER BY 1,2 
+--Low mileage cars with low price
+SELECT TOP 6*
+FROM CH_python$
+WHERE mileage < 100000 AND price < 5000;
